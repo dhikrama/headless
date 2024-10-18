3c012c471328b21c04649bbd05a56572e768a868
Hollie Duncan <hollie@contento.io>
2024-10-18 13:06:44 +0100

    Fixes Mobile Nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,15 @@
 import { BlockData, ContentData } from '@gocontento/client'
 import MogalLogo from '@/images/MogalLogo'
 import Link from 'next/link'
-import { Disclosure } from '@headlessui/react'
+import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid'
 import { usePathname } from 'next/navigation'
 import { classNames } from '@/utils/ClassNames'
+import { useState } from 'react'
 import CategoryPill from './blocks/blog/CategoryPill'
-
-function Logo() {
-  return (
-    <Link
-      href="/"
-      className="inline-block w-[128px] hover:opacity-80 lg:w-[120px]"
-    >
-      <MogalLogo className="h-auto w-full" />
-    </Link>
-  )
-}
+import LinkedInIcon from './icons/LinkedInIcon'
+import TwitterIcon from './icons/TwitterIcon'
+import MogalLogoOutline from '@/images/MogalLogoOutline'
 
 export default function Header({
   mainNav,
@@ -28,107 +21,159 @@ export default function Header({
   categoryLinks: ContentData[]
 }) {
   const pathName = usePathname()
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <Disclosure as="nav">
-      {({ open }) => (
-        <>
-          <div className="mx-auto px-4 sm:px-6 md:px-16">
-            <div className="flex h-28 items-center justify-between">
-              {/* Logo */}
-              <div className="flex flex-shrink-0 items-center gap-x-8">
-                <Logo />
-                <div className="hidden flex-wrap items-center gap-x-3 gap-y-4 md:flex">
-                  {categoryLinks.map((category, index) => (
-                    <>
-                      <CategoryPill
-                        key={`article-category-${index}`}
-                        category={category}
-                      />
-                      <span className="last:hidden">/</span>
-                    </>
-                  ))}
-                </div>
-              </div>
-              <div>
-                {/* Desktop Nav */}
-                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-9">
-                  {mainNav.fields.nav_links.blocks.map((item: BlockData) => {
-                    return (
-                      <Disclosure.Button
-                        key={item.fields.link_text.text}
-                        as={Link}
+    <header className="bg-neutral-100">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex h-20 items-center justify-between px-4 sm:px-6 md:px-16 lg:h-32"
+      >
+        <div className="flex gap-x-9 lg:flex-1">
+          <Link href="/" className="inline-block w-[120px] hover:opacity-80">
+            <span className="sr-only">Mogal</span>
+            <MogalLogo className="h-auto w-full" />
+          </Link>
+          <div className="hidden flex-wrap items-center gap-x-3 gap-y-4 md:flex">
+            {categoryLinks.map((category, index) => (
+              <>
+                <CategoryPill
+                  key={`article-category-${index}`}
+                  category={category}
+                />
+                <span className="last:hidden">/</span>
+              </>
+            ))}
+          </div>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-neutral-900"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="hidden items-center lg:flex lg:gap-x-12">
+          {mainNav.fields.nav_links.blocks.map((item: BlockData) => {
+            if (item.fields.button.is_on) {
+              return (
+                <Link
+                  key={item.fields.link_text.text}
+                  href={item.fields.link_url.text}
+                  className="my-9 inline-block rounded-md bg-neutral-900 px-9 py-3 text-neutral-50 hover:opacity-80"
+                  target={item.fields.open_in_new_tab.is_on ? '_blank' : ''}
+                >
+                  {item.fields.link_text.text}
+                </Link>
+              )
+            } else {
+              return (
+                <Link
+                  key={item.fields.link_text.text}
+                  href={item.fields.link_url.text}
+                  className={classNames(
+                    pathName.startsWith(item.fields.link_url.text)
+                      ? 'underline'
+                      : 'text-neutral-900',
+                    'text-md ',
+                  )}
+                  target={item.fields.open_in_new_tab.is_on ? '_blank' : ''}
+                >
+                  {item.fields.link_text.text}
+                </Link>
+              )
+            }
+          })}
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-neutral-900 px-4">
+          <div className="flex h-20 items-center justify-between">
+            <Link
+              href="/"
+              className="inline-block w-[128px]"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Enigma</span>
+              <MogalLogoOutline className="h-auto w-[120px] w-full" />
+            </Link>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="text-neutral-900"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon
+                aria-hidden="true"
+                className="h-6 w-6 text-neutral-50"
+              />
+            </button>
+          </div>
+          <div>
+            <div className="flex flex-wrap items-center gap-x-3 gap-y-4">
+              {categoryLinks.map((category, index) => (
+                <>
+                  <CategoryPill
+                    key={`article-category-${index}`}
+                    category={category}
+                    onClick={() => setMobileMenuOpen(false)}
+                  />
+                  <span className="py-6 text-neutral-50 last:hidden">/</span>
+                </>
+              ))}
+            </div>
+            <div className="flex flex-col space-y-5 border-t border-t-neutral-50 py-6">
+              {mainNav.fields.nav_links.blocks.map((item: BlockData) => {
+                if (item.fields.button.is_on) {
+                  return (
+                    <div key={item.fields.link_text.text} className="pt-7">
+                      <Link
                         href={item.fields.link_url.text}
-                        className={classNames(
-                          pathName.startsWith(item.fields.link_url.text)
-                            ? 'text-teal-500'
-                            : 'text-neutral-900 hover:opacity-80',
-                          'text-md',
-                        )}
+                        onClick={() => setMobileMenuOpen(false)}
+                        className="inline-block rounded-md bg-neutral-900 px-9 py-3 text-center text-neutral-50"
                         target={
                           item.fields.open_in_new_tab.is_on ? '_blank' : ''
                         }
                       >
                         {item.fields.link_text.text}
-                      </Disclosure.Button>
-                    )
-                  })}
-                </div>
-                <div className="-ml-2 mr-2 flex items-center md:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center text-zinc-600">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <Disclosure.Panel className="absolute w-full bg-zinc-100 md:hidden">
-            {/* Mobile Nav */}
-            <div className="space-y-4 px-4 pb-6 pt-2 sm:px-3">
-              {mainNav.fields.nav_links.blocks.map((item: BlockData) => {
-                if (item.fields.button.is_on) {
-                  // This is a toggle in the navLink block in the CMS - it turns the link into a button
-                  return (
-                    <Disclosure.Button
-                      key={item.fields.link_text.text}
-                      as={Link}
-                      href={item.fields.link_url.text}
-                      className="block bg-neutral-900 px-12 py-3 text-center font-semibold text-neutral-50 hover:opacity-80"
-                      target={item.fields.open_in_new_tab.is_on ? '_blank' : ''}
-                    >
-                      {item.fields.link_text.text}
-                    </Disclosure.Button>
+                      </Link>
+                    </div>
                   )
                 } else {
                   return (
-                    <Disclosure.Button
+                    <Link
                       key={item.fields.link_text.text}
-                      as={Link}
                       href={item.fields.link_url.text}
+                      onClick={() => setMobileMenuOpen(false)}
                       className={classNames(
                         pathName.startsWith(item.fields.link_url.text)
-                          ? 'text-teal-500'
-                          : 'text-zinc-600 hover:opacity-80',
-                        'text-md block font-semibold',
+                          ? 'underline decoration-1 underline-offset-1'
+                          : 'underline-none',
+                        'text-md text-neutral-50',
                       )}
                       target={item.fields.open_in_new_tab.is_on ? '_blank' : ''}
                     >
                       {item.fields.link_text.text}
-                    </Disclosure.Button>
+                    </Link>
                   )
                 }
               })}
             </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+          </div>
+          <div className="flex gap-x-4  text-neutral-50">
+            <LinkedInIcon href="https://www.linkedin.com" />
+            <TwitterIcon href="https://www.twitter.com" />
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
   )
 }
--- components/blocks/blog/CategoryPill.tsx
+++ components/blocks/blog/CategoryPill.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link'
 import { ContentData } from '@gocontento/client'
+import { MouseEventHandler } from 'react'
 
-export default function CategoryPill({ category }: { category: ContentData }) {
+export default function CategoryPill({
+  category,
+  onClick,
+}: {
+  category: ContentData
+  onClick?: MouseEventHandler<HTMLAnchorElement> | undefined
+}) {
   return (
     category && (
       <Link
         href={`/${category.uri}`}
-        className="hover:bold p-2 font-mono text-sm"
+        onClick={onClick}
+        className="hover:bold p-2 font-mono text-sm text-neutral-50 first:pl-0 md:text-neutral-900"
       >
         {category.name}
       </Link>
